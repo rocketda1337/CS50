6cafd49d96c75a69d1546a7a63ecf1e0f2cb5e13
rakett <drumnets@gmail.com>
2019-05-07 19:49:39 +0300

    Create caesar.c
    My solution to CS50 Week 2 Caesar PSET.
    
    I have added extra functionality to the required specs in course:
    You can run the program with either 1 argument, where it will then prompt for message to decipher from you
    
    OR
    
    You can run the program with two arguments, where 2nd argument is your message.

---
+++ Week 2/caesar.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include <cs50.h>
+
+#include <string.h>
+
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    string msg = argv[2];
+    if (argc <= 1)
+    {
+        printf("enter key value!\n");
+        return 1;
+    }
+    if (argc == 2)
+    {
+        msg = get_string("message to cipher:\n");
+    }
+    else if (argc != 3)
+    {
+        printf("Usage ./caesar <key>\n");
+        printf("OPTINAL: 2nd argument message\n");
+        printf("Key must be 1 or more\n");
+        return 1;
+    }
+    int key = atoi(argv[1]);
+    printf("ciphertext: ");
+    for (int i = 0, n = strlen(msg); i < n; i++)
+    {
+        if
+            islower(msg[i])
+                printf("%c", (((msg[i] + key) - 97) % 26) + 97);
+        else if
+            isupper(msg[i])
+                printf("%c", (((msg[i] + key) - 65) % 26) + 65);
+        else
+            printf("%c", msg[i]);
+    }
+    printf("\n");
+}
